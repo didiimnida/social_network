b3cd4d546a5cda8ff09e6084b50bb6b15f9f989e
Diana Hilton <diana.hilton@gmail.com>
2014-09-18 21:07:06 -0700

    Added thumbnails and changed starter data.

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Implementing a basic social network app using Ruby (no Rails), SQL, Rack and ORM
 This social network application will be able to login, register and authenticate users.
 
 Users can update a status, add/accept friends, comment on friends statuses.  
+
+Run.  From the command line, run "config".  The server will be up and running.  
+
+WARNING:  If you want to save the database after turning off the server, please comment out the following methods before restarting: 
+1. delete_database_if_it_exists()
+2. load_schema()
+3. load_data()
+You will find the method calls in "main.rb" if you use (command-f) to search.
+Note:  Will fix this in future versions once I am done testing the code and adding database features.    
+
+Visit [Trello](https://trello.com/b/HpjBCWks/social-network) to see the planning behind the development process.   
--- sql/data.sql
+++ sql/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO
     users (email, password, username, interests, mobile, picture)
 VALUES
-    ('diana@gmail.com', 'a', 'Diana Hilton', 'I am a web developer!', '8165171305', 'http://www.dianahilton.com/wp-content/uploads/2014/09/diana2-e1411090712935.jpg'),
-    ('grace@gmail.com', 'a', 'Grace Hopper', 'Computer Scientist', '8165171305', 'http://www.dianahilton.com/wp-content/uploads/2014/09/hopper-e1411090894202.jpg'),
-    ('max@gmail.com', 'a', 'Max Cantor', 'Programmer. Instructor. NYC.', '8165171305', 'http://www.dianahilton.com/wp-content/uploads/2014/09/max-e1411090884662.png'),
-    ('albert@gmail.com', 'a', 'Albert Einstein', 'I am a physicist.', '8165171305', 'http://www.dianahilton.com/wp-content/uploads/2014/09/biggereinstein-e1411090591720.jpg')
+    ('diana@gmail.com', 'password', 'Diana Hilton', 'I am a web developer!', '8165171305', 'http://www.dianahilton.com/wp-content/uploads/2014/09/diana2-e1411090712935.jpg'),
+    ('grace@gmail.com', 'password', 'Grace Hopper', 'Grace Hopper was an American computer scientist and United States Navy rear admiral.  A pioneer in the field, she was one of the first programmers of the Harvard Mark I computer, invented the first compiler for a computer programming language and popularized the idea of machine-independent programming languages, which led to the development of COBOL.', '8165171305', 'http://www.dianahilton.com/wp-content/uploads/2014/09/hopper-e1411090894202.jpg'),
+    ('max@gmail.com', 'password', 'Max Cantor', 'Programmer. Instructor. ', '8165171305', 'http://www.dianahilton.com/wp-content/uploads/2014/09/max-e1411090884662.png'),
+    ('albert@gmail.com', 'password', 'Albert Einstein', 'Any intelligent fool can make things bigger, more complex, and more violent. It takes a touch of genius -- and a lot of courage -- to move in the opposite direction.', '8165171305', 'http://www.dianahilton.com/wp-content/uploads/2014/09/biggereinstein-e1411090591720.jpg')
 ;
 
 INSERT INTO
@@ -12,12 +12,12 @@ INSERT INTO
 VALUES
     (1, 'I need to do some TDD.', "2014-09-15 01:01:43"),
     (1, 'It is difficult to join/reference database tables without object relational mapping.', "2014-09-15 09:01:44"),
-    (2, 'There is a bug in my computer?', "2014-09-16 06:01:45"),
+    (2, 'There is a bug in my computer.', "2014-09-16 06:01:45"),
     (4, 'The true sign of intelligence is not knowledge but imagination.', "2014-09-16 10:01:46"),
     (3, 'I am going to torture my GA students today! Muwahaha.', "2014-09-17 15:01:47"),
     (1, 'If you want to link to the same action from multiple views, do not try to redirect or you will get stuck in a redirect loop.  Use a variable and render the page again.', "2014-09-17 17:01:48"),
     (1, 'Implementation of the Twilio API was quick, but cannot send to a user number without verifying through Twilio first. Freemium.', "2014-09-17 18:01:49"),
-    (1, 'Oh, no...I really need to add in the network features.  This is a really interesting mapping/joining problem.', "2014-09-17 00:01:50"),
+    (1, 'I need to add in a join table for friendships.  This is an interesting bidirectional network mapping problem.', "2014-09-17 00:01:50"),
     (1, 'My biggest problem is that I did not have a good mental model in my head for the App class so made use of local variables instead of cookies.', "2014-09-18 16:01:50")
 ;
 
@@ -25,11 +25,13 @@ INSERT INTO
     comments (commenter_id, status_id, comment, date)
 VALUES
     (2, 1, 'Test. Test. And test again!', "2014-09-17 01:02:43"),
-    (4, 3, 'Squash it!', "2014-09-17 01:02:43"),
-    (3, 1, 'What is TDD? LOLOLOL.', "2014-09-17 09:02:44"),
+    (4, 3, 'Kill it!', "2014-09-17 01:02:43"),
+    (3, 1, 'I love TDD!!!', "2014-09-17 09:02:44"),
     (1, 4, 'I cannot imagine a world without the internet...', "2014-09-18 06:02:45"),
-    (2, 5, 'Get them, Max!', "2014-09-18 06:02:45"),
-    (1, 5, 'Noooooo.', "2014-09-18 06:02:45")
+    (2, 5, 'Take it to the max, Max!', "2014-09-18 06:02:45"),
+    (1, 5, 'Boo.', "2014-09-18 06:02:45"),
+    (3, 9, 'Did you ask the rubber duck for help?', '2014-09-19 00:02:45'),
+    (4, 7, 'Need those dolla, dolla bills, girl!', '2014-09-19 00:02:45')
 ;
 
 INSERT INTO
